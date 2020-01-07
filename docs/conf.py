e7782f8a45ba2aca4a157ecb64e8e90186780254
smalloranges <smalloranges.lee@gmail.com>
2020-01-07 18:55:31 +0800

    style bug fixed

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ html_favicon = 'resources/favicon.ico'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('css/custom.css?v=20191219')
+    app.add_stylesheet('css/custom.css?v=20200107')
 
 # -- Options for HTMLHelp output ------------------------------------------
 
